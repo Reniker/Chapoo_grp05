0feb59840b710f2eabcee90ac20dee3eaa2cf246
reniker <renik_nike@live.nl>
2018-06-06 15:18:29 +0200

    Add project files.

---
+++ Chapoo_grp05/DAL/DAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Chapoo_grp05.DAL
+{
+    class DAL
+        {//Made By Mark Kea
+            public SqlConnection dbconnection()
+            {
+                string connString = ConfigurationManager.ConnectionStrings["ChapooConnectionString"].ConnectionString;
+                SqlConnection sqls = new SqlConnection(connString);
+
+                return sqls;
+            }
+        }
+    }
